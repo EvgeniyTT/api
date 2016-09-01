7940205e36bbcd2d7d267c274e94a31331f797bb
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-09-01 11:36:40 +0300

    separate db and router from server

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,101 +1,84 @@
-// const app = require('../../server');
+const app = require('../../server');
 const express = require('express');
+
 const router = express.Router();
-
-const db = require('../../lib/mongoose').db;
-// db.on('error', (err) => { console.log(err); });
-// db.once('open', () => {
-
-// module.exports = (app) => {
-
-  // const Image = app.db.model('Image');
-
-  const Image = db.model('Image');
+const Image = app.db.model('Image');
 
   // routing
-  router.get('/', (req, res, next) => {
-    Image.find((err, images) => {
-      if (err) { return next(err);
-      } else if (!images) {
-        err = new Error('No images were found');
-        err.status = 404;
-        return next(err);
-      }
-      res.json(images);
-    });
-  });
-
-  router.get('/:imageId', (req, res, next) => {
-    const imageId = req.params.imageId;
-    Image.findOne({ _id: imageId }, (err, image) => {
-      if (err) { return next(err);
-      } else if (!image) {
-        err = new Error('Image was not found');
-        err.status = 404;
-        return next(err);
-      }
-      res.json(image);
-    });
-  });
-
-  router.post('/', (req, res, next) => {
-    if (req.body.src === '' || req.body.description === '') {
-      const err = new Error('Required fields are not populated');
-      err.status = 400;
+router.get('/images', (req, res, next) => {
+  Image.find((err, images) => {
+    if (err) { return next(err);
+    } else if (!images) {
+      err = new Error('No images were found');
+      err.status = 404;
       return next(err);
     }
-    const image = new Image(req.body);
-    image.save((err) => {
-      if (err) { return next(err); }
-      res.json(image);
-    });
+    res.json(images);
   });
+});
 
-  router.put('/:imageId', (req, res, next) => {
-    if (req.body.src === '' || req.body.description === '') {
-      const err = new Error('Required fields are not populated');
-      err.status = 400;
+router.get('/images/:imageId', (req, res, next) => {
+  const imageId = req.params.imageId;
+  Image.findOne({ _id: imageId }, (err, image) => {
+    if (err) { return next(err);
+    } else if (!image) {
+      err = new Error('Image was not found');
+      err.status = 404;
       return next(err);
     }
-    const imageId = req.params.imageId;
-    const image = req.body;
-    Image.findOneAndUpdate({ _id: imageId }, image, (err, image) => {
-      if (err) { return next(err);
-      } else if (!image) {
-        err = new Error('Image was not found');
-        err.status = 404;
-        return next(err);
-      }
-      res.json(image);
-    });
+    res.json(image);
   });
+});
 
-  router.delete('/:imageId', (req, res, next) => {
-    const imageId = req.params.imageId;
-    Image.remove({ _id: imageId }, (err, image) => {
-      if (err) { return next(err);
-      } else if (!image) {
-        err = new Error('Image was not found');
-        err.status = 404;
-        return next(err);
-      }
-      res.json(image);
-    });
+router.post('/images', (req, res, next) => {
+  if (req.body.src === '' || req.body.description === '') {
+    const err = new Error('Required fields are not populated');
+    err.status = 400;
+    return next(err);
+  }
+  const image = new Image(req.body);
+  image.save((err) => {
+    if (err) { return next(err); }
+    res.json(image);
   });
+});
+
+router.put('/images/:imageId', (req, res, next) => {
+  if (req.body.src === '' || req.body.description === '') {
+    const err = new Error('Required fields are not populated');
+    err.status = 400;
+    return next(err);
+  }
+  const imageId = req.params.imageId;
+  const image = req.body;
+  Image.findOneAndUpdate({ _id: imageId }, image, (err, image) => {
+    if (err) { return next(err);
+    } else if (!image) {
+      err = new Error('Image was not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.json(image);
+  });
+});
+
+router.delete('/images/:imageId', (req, res, next) => {
+  const imageId = req.params.imageId;
+  Image.remove({ _id: imageId }, (err, image) => {
+    if (err) { return next(err);
+    } else if (!image) {
+      err = new Error('Image was not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.json(image);
+  });
+});
 
   // error handling
-  router.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send(err.toString());
-  });
-
-// });
-
-console.log('------------------');
-console.log(router);
-console.log('------------------');
-
-//   return router;
-// };
+router.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.toString());
+});
 
 module.exports = router;
